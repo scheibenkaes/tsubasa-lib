942c985267d8e548db86894424ac9b7eba73332f
Benjamin Klüglein <scheibenkaes@googlemail.com>
2014-01-16 17:17:56 +0100

    update readme and project.clj

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # tsubasa-lib
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to manage table football data.
 
 ## Usage
 
-FIXME
+TBD
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 Benjamin Klüglein
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tsubasa-lib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a lib to manage table football data"
+  :url "http://github.com/scheibenkaes/tsubasa-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
