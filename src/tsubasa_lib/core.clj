d93b61d7df2435648aef845f8fae2387ec4b0596
Benjamin Klüglein <scheibenkaes@googlemail.com>
2014-01-22 19:35:17 +0100

    add data schemas and generator fns

--- src/tsubasa_lib/core.clj
+++ src/tsubasa_lib/core.clj
@@ -1,6 +1,38 @@
-(ns tsubasa-lib.core)
+(ns tsubasa-lib.core
+  (:require [schema.core :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+;; schemas
+(def Name s/Str)
+
+(def Goal {:for-team Name})
+
+(def Player {:name Name})
+
+(def Team {:name Name
+           :players [Player]})
+
+(def Match {:teams [Team]
+            :goals [Goal]})
+
+;; generator fns
+
+(defn match
+  "create a instance of Match"
+  [team1 team2]
+  {:teams [team1 team2]
+   :goals []})
+
+(defn team [name players]
+  "create a instance of Team"
+  {:name name :players players})
+
+(defn player
+  "create a new instance of Player"
+  [name]
+  {:name name})
+
+(defn goal
+  "create a new instance of Goal"
+  [team]
+  {:for-team team})
--- test/tsubasa_lib/core_test.clj
+++ test/tsubasa_lib/core_test.clj
@@ -1,7 +1,29 @@
 (ns tsubasa-lib.core-test
   (:require [clojure.test :refer :all]
-            [tsubasa-lib.core :refer :all]))
+            [tsubasa-lib.core :refer :all]
+            [schema.core :as s]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn obeys-schema [schema data]
+  (is (= (s/validate schema data) data)))
+
+(deftest test-goal
+  (testing "goal returns a valid Goal"
+    (obeys-schema Goal (goal "test"))))
+
+(deftest test-player
+  (testing "player returns a valid Player"
+    (obeys-schema Player (player "Test"))))
+
+(deftest test-team
+  (testing "returns valid team"
+    (let [name "test team"
+          players [(player "1") (player "2")]]
+      (obeys-schema Team (team name players)))))
+
+(deftest test-match
+  (testing "returns valid match"
+    (let [team1 (team "t1" [(player "1") (player "2")])
+          team2 (team "t2" [(player "t2p1") (player "t2p2")])
+          data (match team1 team2)]
+      (obeys-schema Match data))))
+
